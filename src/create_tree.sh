747cee06102eb769a75d0c37c507f17bb6da0f8c
Markus Hoffmann <kollo@users.sourceforge.net>
2020-01-19 06:00:15 +0100

    Added --prefix option to hddashgen, autogenerate remote dash trees.

--- src/create_tree.sh
+++ src/create_tree.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Shell script to create the initial local folder in 
 # $(Home)/.hyperdash/dashboards
 # copy the system files to the local folder and 
@@ -7,20 +8,24 @@
 #
 #
 mosquitto_pub -h localhost -t MQTT_HYPERDASH/STATUS_SM -m "preparing directories..." -r
-
 mkdir -p ~/.hyperdash/dashboards
-
 cp /usr/share/hyperdash/dashboards/* ~/.hyperdash/dashboards/
-
 cd ~/.hyperdash/dashboards/
 
-mosquitto_pub -h localhost -t MQTT_HYPERDASH/STATUS_SM -m "generating dashboards..." -r
 
+
+if [ "$#" -le 0 ]; then
+mosquitto_pub -h localhost -t MQTT_HYPERDASH/STATUS_SM -m "generating local dashboards..." -r
 mqtt-list-topics | sort | hddashgen
+fi
+if [ "$#" -eq 1 ]; then
+BROKER=`mosquitto_sub -h localhost -t "MQTT_HYPERDASH/BROKER_SC" -C 1`
+BASEBROKER="${BROKER##*/}"
+BASEBROKER="${BASEBROKER%%:*}"
+mosquitto_pub -h localhost -t MQTT_HYPERDASH/STATUS_SM -m "generating remote dashboards..." -r
+ mqtt-list-topics --broker $BROKER | sort | hddashgen --broker $BROKER --prefix $BASEBROKER
 
-
-
-
+fi
 
 
 mosquitto_pub -h localhost -t MQTT_HYPERDASH/STATUS_SM -m "done." -r
